5b340752e20f7ab2d94a4ea9b4ac654212b531a9
YanhangLi <114519920+YanhangLi@users.noreply.github.com>
2023-04-10 19:08:55 -0700

    Add exercise 5

---
+++ exercise/exercise 5/SellTicket.java
@@ -0,0 +1,3 @@
+public interface SellTicket {
+    void buyTicket();
+}
---
+++ exercise/exercise 5/TestMain2.java
@@ -0,0 +1,9 @@
+public class TestMain2 {
+    public static void main(String[] args) {
+        TrainStation trainStation = new TrainStation();
+        trainStation.buyTicket();
+
+        ConsignmentStation consignmentStation = new ConsignmentStation();
+        consignmentStation.buyTicket();
+    }
+}
